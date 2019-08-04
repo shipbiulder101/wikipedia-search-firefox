0d180f718c421bbdce4b47c731138e102dd02b25
shipbiulder101 <cubecubedproductionsuk@gmail.com>
2019-08-04 09:33:43 +0100

    Added search toolss

--- background.js
+++ background.js
@@ -1,6 +1,20 @@
 //create menu items
 browser.menus.create({
-    id: "seach-wikipedia",
+    id: "search-wikipedia",
     title: "Search Wikipedia for selected text",
     contexts: ["Selection"]
 });
+
+//handler for clicks
+browser.menus.onClicked.addListener(info, tab) => {
+    switch(info.menuItemId){
+        case "search-wikipedia":
+            console.log("Search initiated");
+            var sel = Window.getSelection;
+            var searchTerm = sel.toString();
+            var creating = browser.tabs.create({
+                url: "https://en.wikipedia.org/w/index.php?search=" + searchTerm,
+                active: true
+            })
+    }
+}
